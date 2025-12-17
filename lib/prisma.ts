e520e38516e34c6abd4724d8bb770e9e2d6253e7
Saurav Kumar <codewizard.saurav@gmail.com>
2025-12-17 13:19:18 +0530

    migrated to prisma 7

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../prisma/generated/prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 
 const connectionString = `${process.env.DATABASE_URL}`;
